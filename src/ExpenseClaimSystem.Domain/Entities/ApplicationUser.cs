62bcf929f48ceb8ac89a43f184e2645c9eabdd8d
Dinindu Sandaruwan <dinindu.sandaruwan93@gmail.com>
2026-02-25 12:13:52 +0530

    cleaning up solution

--- src/ExpenseClaimSystem.Domain/Entities/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace ExpenseClaimSystem.Domain.Entities
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public string FullName { get; set; }
-    }
-}
